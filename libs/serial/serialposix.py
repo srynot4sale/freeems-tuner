53094f29e0630ab61d2fafb1a403165f001ee630
Aaron Barnes <aaronb@catalyst.net.nz>
2009-09-18 10:51:50 +1200

    Compatibility fix for Python 2.6

--- libs/serial/serialposix.py
+++ libs/serial/serialposix.py
@@ -416,7 +416,7 @@ class Serial(SerialBase):
         """Set terminal status line: Request To Send"""
         if self.fd is None: raise portNotOpenError
         if level:
-            fcntl.ioctl(self.fd, TIOCMBIS, TIOCM_RTS_str)
+            fcntl.ioctl(self.fd, TIOCMBIS, int(TIOCM_RTS_str))
         else:
             fcntl.ioctl(self.fd, TIOCMBIC, TIOCM_RTS_str)
 
@@ -424,7 +424,7 @@ class Serial(SerialBase):
         """Set terminal status line: Data Terminal Ready"""
         if self.fd is None: raise portNotOpenError
         if level:
-            fcntl.ioctl(self.fd, TIOCMBIS, TIOCM_DTR_str)
+            fcntl.ioctl(self.fd, TIOCMBIS, int(TIOCM_DTR_str))
         else:
             fcntl.ioctl(self.fd, TIOCMBIC, TIOCM_DTR_str)
 
